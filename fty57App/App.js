8b1cc93c46512571aa1a07979b4a17030da0b325
Faitly <fty.57@alu.ufc.br>
2021-10-09 13:36:44 -0300

    20. Sistema de módulos do ECMAScript

--- fty57App/App.js
+++ fty57App/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { View } from 'react-native';
+
+import CompPadrao, { Comp1, Comp2} from "./src/components/Multi.js"
 import Primeiro from "./src/components/Primeiro"
 
 export default function App() {
   return (
     <View>
+      <CompPadrao/>
+      <Comp1/>
+      <Comp2/>
       <Primeiro/>
     </View> 
   )
