41bda3a870953702e5f3654c0c05c11943ec476d
Faitly <fty.57@alu.ufc.br>
2021-10-18 09:36:26 -0300

    53. Componente Mega-Sena #01

--- fty57App/src/mega/Mega.js
+++ fty57App/src/mega/Mega.js
@@ -1,22 +1,31 @@
 import React, { Component } from "react"
-import { Text, TextInput } from "react-native"
+import { Text, TextInput, Button } from "react-native"
 import style from "../components/estilo"
 
 export default class Mega extends Component {
 
      state = {
-          qtdeNumeros: this.props.qtdeNumeros 
-     }
-
-     constructor(props) {
-          super(props)
-          this.alterarQtdeNumero = this.alterarQtdeNumero.bind(this)
+          qtdeNumeros: this.props.qtdeNumeros,
+          numeros : []
      }
 
      alterarQtdeNumero = (qtde) =>{
-          this.setState({qtdeNumeros})
+          this.setState({qtdeNumeros: + qtde})
      }
 
+     gerarNumeroNaoContido = nums => {
+          const novo = parseInt(Math.random() * 60) + 1
+          // Esse código lhe fornece um valor aleatório entre 1 e 60
+          return nums.includes(novo) ? this.gerarNumeroNaoContido(nums) : novo
+     }
+
+     gerarNumeros = () =>{
+          const numeros = Array(this.state.qtdeNumeros)
+               .fill()
+               .reduce(nums => [...nums, this.gerarNumeroNaoContido(nums)], [])
+               .sort((a,b) => a - b)
+          this.setState({numeros})
+     }
      render() {
           return (
                <>
@@ -28,9 +37,16 @@ export default class Mega extends Component {
                          keyboardType={"number-pad"}
                          style={{borderBottomWidth:1}}
                          placeholder="Qtd de Números"
-                         value = {this.state.qtdeNumeros}
+                         value = {`${this.state.qtdeNumeros}`}
                          onChangeText={qtde => this.alterarQtdeNumero(qtde)}
                     />
+                    <Button 
+                         title="Gerar"
+                         onPress={this.gerarNumeros}
+                    />
+                    <Text>
+                         {this.state.numeros.join(",")}
+                    </Text>
                </>
           )
      }
