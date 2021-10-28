89ff677c2b6c86f1d2a1f8c225887a355c992c9e
Faitly <fty.57@alu.ufc.br>
2021-10-28 08:41:15 -0300

    83. Instalando as Dependências

--- fty57App/App.js
+++ fty57App/App.js
@@ -1,71 +1,10 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet , Text } from 'react-native';
 
-import Mega from './src/mega/Mega';
-//import FlexboxV4 from "./src/layout/FlexboxV4"
-//import FlexboxV3 from './src/layout/FlexboxV3';
-//import FlexboxV2 from './src/layout/FlexboxV2';
-//import FlexboxV1 from './src/layout/FlexboxV1';
-//import Quadrado from './src/layout/Quadrado';
-//import DigiteSeuNome from './src/DigiteSeuNome';
-//import ListaProdutosV2 from "./src/produtos/ListaProdutosV2"
-//import ListaProdutos from "./src/produtos/ListaProdutos"
-//import UsuarioLogado from './src/components/UsuarioLogado';
-//import Familia from './src/relacao/Familia';
-//import Membro from './src/relacao/Membro';
-//import ParImpar from "./src/components/ParImpar"
-//import Diferenciar from "./src/components/Diferenciar"
-//import ContadorV2 from "./src/contador/ContadorV2"
-//import Pai from "./src/indireta/Pai"
-//import Pai from "./src/direta/Pai"
-//import Contador from "./src/components/Contador"
-//import Botao from './src/components/Botao'
-//import Titulo from "./src/components/Titulo"
-//import Aleatorio from "./src/components/Aleatorio";
-//import MinMax from "./src/components/MinMax"
-//import CompPadrao, { Comp1, Comp2} from "./src/components/Multi.js"
-//import Primeiro from "./src/components/Primeiro"
-
-
-
 export default function App() {
   return (
     <SafeAreaView style={style.App}>
-      <Mega qtdeNumeros = {12}/>
-      {/* 
-      <FlexboxV4/>
-      <FlexboxV3/>
-      <FlexboxV2/>
-      <FlexboxV1/>
-      <DigiteSeuNome/>
-      <ListaProdutosV2/>
-      <ListaProdutos/>
-      <UsuarioLogado usuario={{nome: "Gui", email:"gui@gui.com"}} />
-      <UsuarioLogado usuario={{nome: "Ana"}} />
-      <UsuarioLogado usuario={{email:"gui@gui.com"}} />
-      <Familia>
-          <Membro nome="Bia" sobrenome="Freitas"/>
-          <Membro nome="Carlos" sobrenome="Freitas"/>
-      </Familia>
-      <Familia>
-          <Membro nome="Álisson" sobrenome="Véras"/>
-          <Membro nome="Manuella" sobrenome="Véras"/>
-      </Familia>
-      <ParImpar num={3}/>
-      <Diferenciar/>
-      <ContadorV2/>
-      <Pai/>
-      <Pai/>
-      <Contador inicial={100} passo={13}/>
-      <Botao/>
-      <Titulo principal="Cadastro de Produto" secundario = "Tela de Cadastro do Produto"/>
-      <Aleatorio min={1} max={60}/>
-      <MinMax min={3} max ={20}/>
-      <MinMax min={2} max ={85}/> 
-      <CompPadrao/>
-      <Comp1/>
-      <Comp2/>
-      <Primeiro/>  */}
+
     </SafeAreaView> 
   )
 }
