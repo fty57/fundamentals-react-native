75874b295038f661a070c5bc4646f4540a9e915b
Faitly <fty.57@alu.ufc.br>
2021-10-29 09:21:04 -0300

    89. Navegação Stack #04

--- fty57App/src/components2/PassoStack.js
+++ fty57App/src/components2/PassoStack.js
@@ -3,15 +3,21 @@ import { Button, View, StyleSheet, Text } from 'react-native';
 
 export default props => {
      return (
-          <View style={{flex: 1}}>
-               <View>
+          <View style={{ flex: 1 }}>
+               <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
                     {
-                         props.avancar 
-                         ? <Button title="Avançar" onPress={() => props.navigation.navigate(props.avancar)}/>
-                         : false
+                         props.voltar
+                              ? <Button title="Voltar" onPress={() => props.navigation.goBack()} />
+                              : false
                     }
+                    {
+                         props.avancar
+                              ? <Button title="Avançar" onPress={() => props.navigation.navigate(props.avancar)} />
+                              : false
+                    }
+
                </View>
-               <View style={{flex: 1}}>
+               <View style={{ flex: 1 }}>
                     {props.children}
                </View>
           </View>
